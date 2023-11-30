3324338115d3056ccd68ec58b2af4f22f3adc910
mazacuata1 <139035082+mazacuata1@users.noreply.github.com>
2023-11-30 02:48:41 -0600

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
     apiKey: "AIzaSyB7uP7odmlKLZoqkJlttP5Kbe7bnu2QksE",
     authDomain: "modificador-41f10.firebaseapp.com",
     projectId: "modificador-41f10",
@@ -7,4 +9,4 @@ const firebaseConfig = {
     messagingSenderId: "522920400477",
     appId: "1:522920400477:web:d45244eb92a96625bc5fb4",
     measurementId: "G-TEYPDQ7B50"
-  };
+});
