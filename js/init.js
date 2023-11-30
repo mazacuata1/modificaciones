0530201117531e2484ca051e552a58bb61df35cb
mazacuata1 <139035082+mazacuata1@users.noreply.github.com>
2023-11-30 03:27:36 -0600

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -3,10 +3,10 @@
 // @ts-ignore
 firebase.initializeApp({
     apiKey: "AIzaSyB7uP7odmlKLZoqkJlttP5Kbe7bnu2QksE",
-    authDomain: "modificador-41f10.firebaseapp.com",
-    projectId: "modificador-41f10",
-    storageBucket: "modificador-41f10.appspot.com",
-    messagingSenderId: "522920400477",
-    appId: "1:522920400477:web:d45244eb92a96625bc5fb4",
-    measurementId: "G-TEYPDQ7B50"
+  authDomain: "modificador-41f10.firebaseapp.com",
+  projectId: "modificador-41f10",
+  storageBucket: "modificador-41f10.appspot.com",
+  messagingSenderId: "522920400477",
+  appId: "1:522920400477:web:d45244eb92a96625bc5fb4",
+  measurementId: "G-TEYPDQ7B50"
 });
