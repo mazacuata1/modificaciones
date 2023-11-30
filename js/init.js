6c4b74b119af99d600a937d2f545c248e00c2a08
mazacuata1 <139035082+mazacuata1@users.noreply.github.com>
2023-11-30 02:41:30 -0600

    vsavsad

--- js/init.js
+++ js/init.js
@@ -1,7 +1,5 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
-firebase.initializeApp({
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyB7uP7odmlKLZoqkJlttP5Kbe7bnu2QksE",
     authDomain: "modificador-41f10.firebaseapp.com",
     projectId: "modificador-41f10",
@@ -9,7 +7,4 @@ firebase.initializeApp({
     messagingSenderId: "522920400477",
     appId: "1:522920400477:web:d45244eb92a96625bc5fb4",
     measurementId: "G-TEYPDQ7B50"
-});
-
-// Inicializa Firestore y Auth (opcional, si los usas)
-var db = firebase.firestore();
+  };
