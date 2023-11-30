7587d4e27b4d52a04a71c0a2fef11f25255869ca
mazacuata1 <139035082+mazacuata1@users.noreply.github.com>
2023-11-30 13:54:51 -0600

    Version 7.5

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB7uP7odmlKLZoqkJlttP5Kbe7bnu2QksE",
   authDomain: "modificador-41f10.firebaseapp.com",
   projectId: "modificador-41f10",
@@ -9,6 +9,15 @@ firebase.initializeApp({
   messagingSenderId: "522920400477",
   appId: "1:522920400477:web:d45244eb92a96625bc5fb4",
   measurementId: "G-TEYPDQ7B50"
-});
+};
 
-var db = firebase.firestore();
+
+firebase.initializeApp(config)
+  .then(() => {
+    console.log("Firebase inicializado correctamente");
+  })
+  .catch((error) => {
+    console.error("Error al inicializar Firebase:", error);
+  });
+
+  var db = firebase.firestore();
